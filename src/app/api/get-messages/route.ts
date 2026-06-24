e671e8a71090585736432dae986ea965de1f52fc
Rohit Jadhav <rjdhav67@gmail.com>
2026-06-24 13:38:14 +0530

    Add API routes for user authentication, username uniqueness check, message retrieval, and code verification, along with a new ApiResponse type definition.

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,59 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import UserModel from "@/model/User";
+import dbConnect from "@/lib/dbConnect";
+import { User } from "next-auth";
+import mongoose from "mongoose";
+
+export async function GET(request: Request) {
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user;
+
+    if (!session || !session.user) {
+        return Response.json(
+            {
+                success: false,
+                message: "Unauthorized",
+            },
+            {status: 401}
+        )
+    }
+    const userId = new mongoose.Types.ObjectId(user._id);
+    try {
+        const user = await UserModel.aggregate([
+            {$match: {_id: userId}},
+            {$unwind: "$messages"},
+            {$sort: {'messages.createdAt': -1}},
+            {$group: {_id: "$_id", messages: {$push: "$messages"}}},
+
+        ])
+        if (!user || user.length === 0) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "user not found",
+                },
+                {status: 404}
+            )
+        }
+        return Response.json(
+            {
+                success: true,
+                message: "Messages fetched successfully",
+                messages: user[0].messages,
+            },
+            {status: 200}
+        )
+    } catch (error: any) {
+        console.log("Failed to get messages:", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Failed to get messages",
+            },
+            {status: 500}
+        )
+    }
+  
+}
