0c5a6cd27e79b69fadb3f6731191cba883304723
Rohit Jadhav <rjdhav67@gmail.com>
2026-06-25 23:46:39 +0530

    fix : routes api

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
-export { default } from 'next-auth/middleware';
 
 export const config = {
   matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
@@ -10,8 +9,6 @@ export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
 
-  // Redirect to dashboard if the user is already authenticated
-  // and trying to access sign-in, sign-up, or home page
   if (
     token &&
     (url.pathname.startsWith('/sign-in') ||
